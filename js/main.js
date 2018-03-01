4e322046471f9fc0f8e10948ade488edc83f4f82
Noah Fisher <nfisher@mica.edu>
2018-03-01 16:28:15 -0500

    Uhhh
    2132

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// var lyricNum = 0;
+// var playButton = document.getElementById("playpause");
+// var pressKey = document.getElementById("l1");
+
+// playButton.addEventListener("click", function() {
+//   if (video.paused == true) {
+//     video.play();
+//     document.getElementById("arm").style.transform = "translateY(15%)";
+//     document.getElementById("video").style.opacity = ".5";
+//   } else {
+//     video.pause();
+//     document.getElementById("arm").style.transform = "translateY(0%)";
+//     document.getElementById("video").style.opacity = "0";
+//   }
+// });
+
+// function changeCounter() {
+//   lyricNum++;
+// };
+//
+// if (lyricNum == 0){
+//     document.getElementById("l1").innerHTML = "Villain get the money like curls";
+//   } else if (lyricNum == 1) {
+//     document.getElementById("l1").innerHTML = "They just tryin' to get a nut like squirrels in his mad world";
+//     document.getElementById("mask").style.opacity = "0";
+//   } else if (lyricNum == 2){
+//     document.getElementById("l1").innerHTML = "Land of milk and honey with the swirls";
+//   } else if (lyricNum == 3){
+//     document.getElementById("l1").innerHTML = "Where reckless naked girls get necklaces of pearls";
+// };
+
+// $(document).keydown(function(keyPressed) {
+//  if (keyPressed.keyCode == 40) {
+//    lyricNum++;
+//    console.log(lyricNum);
+//  }
+// });
